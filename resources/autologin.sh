1e1b3abaf6dcad66451d2c5febc3c63cbbb605b0
Rong Bao <rong.bao@csmantle.top>
2026-06-09 16:10:34 +0800

    Tee to stdout/stderr as well as log files

--- resources/autologin.sh
+++ resources/autologin.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
+set -u
+
 if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 <log_file> <srun_log_file>" >&2
+    echo "Usage: USERNAME=... PASSWORD=... $0 <log_file> <srun_log_file>" >&2
     exit 1
 fi
-if [ -z "$USERNAME" ] || [ -z "$PASSWORD" ]; then
-    echo "Error: USERNAME and PASSWORD must be set" >&2
-    exit 1
-fi
+: "${USERNAME:?Error: USERNAME must be set}"
+: "${PASSWORD:?Error: PASSWORD must be set}"
 
 is_down=0
 while true; do
@@ -15,12 +15,14 @@ while true; do
 
     wget -q --spider -T 3 'http://connectivitycheck.platform.hicloud.com/generate_204'
     res="$?"
-    {
-        if [ "$res" -ne 0 ]; then
-                /bin/srun --username="$USERNAME" --password="$PASSWORD" 2>&1
-                printf 'Exit code: %d\n' "$?"
-        fi
-    } >>"$2"
+    if [ "$res" -ne 0 ]; then
+        {
+            {
+                /bin/srun --username="$USERNAME" --password="$PASSWORD"
+                printf '/bin/srun exited with %d\n' "$?"
+            } 2>&1 1>&3 | tee -a "$2" >&2
+        } 3>&1 | tee -a "$2"
+    fi
     {
         date="$(date '+%Y-%m-%d %H:%M:%S')"
         if [ "$res" -ne 0 ] && [ "$is_down" -eq 0 ]; then
@@ -30,5 +32,5 @@ while true; do
             echo "[$date] Network UP"
             is_down=0
         fi
-    } >>"$1"
+    } | tee -a "$1"
 done
