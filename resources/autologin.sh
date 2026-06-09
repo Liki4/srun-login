df3e79e829beafcd069b7b635906dc2f68b2edac
Rong Bao <rong.bao@csmantle.top>
2026-06-09 11:09:13 +0800

    Refactor autologin.sh

--- resources/autologin.sh
+++ resources/autologin.sh
@@ -1,23 +1,34 @@
-#! /bin/bash
+#!/bin/sh
 
-isdown=0
-while [ 1 ]
-do
-    sleep 5
-    ping -c 4 -W 1 223.5.5.5
-    res=$?
-    if [ "$res" != "0" ];then
-        /bin/srun --username=$USERNAME --password=$PASSWORD >> $2
-        echo "" >> $2
-    fi
-    if [[ "$res" != "0" && $isdown == 0 ]];then
-        time=$(date "+%Y-%m-%d %H:%M:%S")
-        echo "[${time}] Network DOWN" >> $1
-        isdown=1
-    fi
-    if [[ "$res" == "0" && $isdown == 1 ]];then
-        time=$(date "+%Y-%m-%d %H:%M:%S")
-        echo "[${time}] Network UP" >> $1
-        isdown=0
-    fi
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 <log_file> <srun_log_file>" >&2
+    exit 1
+fi
+if [ -z "$USERNAME" ] || [ -z "$PASSWORD" ]; then
+    echo "Error: USERNAME and PASSWORD must be set" >&2
+    exit 1
+fi
+
+is_down=0
+while true; do
+    sleep 20
+
+    wget -q --spider -T 3 'http://connectivitycheck.platform.hicloud.com/generate_204'
+    res="$?"
+    {
+        if [ "$res" -ne 0 ]; then
+                /bin/srun --username="$USERNAME" --password="$PASSWORD" 2>&1
+                printf 'Exit code: %d\n' "$?"
+        fi
+    } >>"$2"
+    {
+        date="$(date '+%Y-%m-%d %H:%M:%S')"
+        if [ "$res" -ne 0 ] && [ "$is_down" -eq 0 ]; then
+            echo "[$date] Network DOWN"
+            is_down=1
+        elif [ "$res" -eq 0 ] && [ "$is_down" -eq 1 ]; then
+            echo "[$date] Network UP"
+            is_down=0
+        fi
+    } >>"$1"
 done
